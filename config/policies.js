54f2eb035fedd726b3b54cdb58afdc189bcaa08a
lennyaiko <lennyaiko17@gmail.com>
2024-02-16 18:45:04 +0100

    temp

--- config/policies.js
+++ config/policies.js
@@ -15,8 +15,8 @@ module.exports.policies = {
    * (`true` allows public access)                                            *
    *                                                                          *
    ***************************************************************************/
-  // '*': true,
-  '*': ['share-path', 'is-authenticated'],
+  '*': true,
+  // '*': ['share-path', 'is-authenticated', 'is-approved'],
 
   // Auth
   'auth/signin': true,
