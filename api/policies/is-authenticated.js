d545ac90c659517cc1d802832ec82a8b6481692b
lennyaiko <lennyaiko17@gmail.com>
2024-02-16 18:13:38 +0100

    fix

--- api/policies/is-authenticated.js
+++ api/policies/is-authenticated.js
@@ -1,6 +1,6 @@
 module.exports = async function (req, res, proceed) {
-  if (!req.session.userId) {
-    return res.redirect('/signin')
+  if (req.session.userId) {
+    proceed()
   }
-  proceed()
+  return res.redirect('/signin')
 }
