29eef64eca9b37e5b6f10516802f35b9e4598788
lennyaiko <lennyaiko17@gmail.com>
2024-02-25 19:35:41 +0100

    testing

--- api/policies/is-authenticated.js
+++ api/policies/is-authenticated.js
@@ -1,7 +1,11 @@
 module.exports = async function (req, res, proceed) {
+  console.log(req.session.userId)
+  console.log(req.session)
+  console.log(res.props)
   if (req.session.userId) {
     return proceed()
   }
-  req.flash('message', 'Please login first')
-  return res.redirect('/signin')
+  // req.flash('message', 'Please login first')
+  // return res.redirect('/signin')
+  return proceed()
 }
