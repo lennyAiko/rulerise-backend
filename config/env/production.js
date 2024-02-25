299760f71950ea513e07c25a215f353b72871ae7
lennyaiko <lennyaiko17@gmail.com>
2024-02-25 18:14:44 +0100

    testing

--- config/env/production.js
+++ config/env/production.js
@@ -180,6 +180,11 @@ module.exports = {
      * http://sailsjs.com/config/session#?the-session-id-cookie                 *
      *                                                                          *
      ***************************************************************************/
+
+    secret: process.env.REDIS_SECRET,
+    adapter: '@sailshq/connect-redis',
+    url: process.env.REDIS_URL,
+    ssl: true,
     cookie: {
       secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
