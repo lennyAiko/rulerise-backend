69655747a41c87caebb409acc3eed54222daec87
lennyaiko <lennyaiko17@gmail.com>
2024-03-25 20:58:52 +0100

    some fixes

--- api/controllers/api/webhook.js
+++ api/controllers/api/webhook.js
@@ -54,72 +54,77 @@ module.exports = {
     if (event.type === 'checkout.session.completed') {
     }
 
-    let customer
     let mostRecentApplication
     switch (event.type) {
       case 'payment_intent.succeeded':
         const paymentIntentSucceeded = event.data.object
-        customer = await stripe.customers.retrieve(event.data.object.customer)
-        await sails.helpers.sendEmail(
-          customer.metadata.email,
-          {
-            amount: paymentIntentSucceeded.amount.toLocaleString('en-CA', {
-              style: 'currency',
-              currency: 'CAD',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            }),
-            date: formatDate(paymentIntentSucceeded.created * 1000),
-            course: customer.metadata.course,
-          },
-          'Payment Successful',
-          'email-payment-done',
-          false
-        )
-        mostRecentApplication = await Application.findOne({
-          where: { firstName: customer.metadata.firstName },
-          sort: 'createdAt DESC', // Sort by createdAt in descending order to get the most recent record
-        })
+        stripe.customers
+          .retrieve(paymentIntentSucceeded.customer)
+          .then(async (customer) => {
+            await sails.helpers.sendEmail(
+              customer.metadata.email,
+              {
+                amount: (paymentIntentSucceeded.amount / 100).toLocaleString(
+                  'en-CA',
+                  {
+                    style: 'currency',
+                    currency: 'CAD',
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  }
+                ),
+                date: formatDate(paymentIntentSucceeded.created * 1000),
+                course: customer.metadata.course,
+              },
+              'Payment Successful',
+              'email-payment-done',
+              false
+            )
 
-        if (mostRecentApplication) {
-          await Application.updateOne({ id: mostRecentApplication.id }).set({
-            status: 'accepted',
+            Application.find({
+              where: { firstName: customer.metadata.firstName },
+              sort: 'createdAt DESC',
+            })
+              .then(async (application) => {
+                await Application.updateOne({ id: application[0].id }).set({
+                  status: 'accepted',
+                })
+              })
+              .catch((err) => sails.log(err))
           })
-        } else {
-          // Handle case where no matching record is found
-        }
         break
       case 'payment_intent.payment_failed':
         const paymentIntentFailed = event.data.object
-        customer = await stripe.customers.retrieve(event.data.object.customer)
-        await sails.helpers.sendEmail(
-          customer.metadata.email,
-          {
-            amount: paymentIntentFailed.amount.toLocaleString('en-CA', {
-              style: 'currency',
-              currency: 'CAD',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            }),
-            date: formatDate(paymentIntentFailed.created * 1000),
-            course: customer.metadata.course,
-          },
-          'Payment Failed',
-          'email-payment-failed',
-          false
-        )
-        mostRecentApplication = await Application.findOne({
-          where: { firstName: customer.metadata.firstName },
-          sort: 'createdAt DESC', // Sort by createdAt in descending order to get the most recent record
-        })
-
-        if (mostRecentApplication) {
-          await Application.updateOne({ id: mostRecentApplication.id }).set({
-            status: 'rejected',
+        stripe.customers
+          .retrieve(paymentIntentFailed.customer)
+          .then(async (customer) => {
+            await sails.helpers.sendEmail(
+              customer.metadata.email,
+              {
+                amount: paymentIntentSucceeded.amount.toLocaleString('en-CA', {
+                  style: 'currency',
+                  currency: 'CAD',
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                }),
+                date: formatDate(paymentIntentSucceeded.created * 1000),
+                course: customer.metadata.course,
+              },
+              'Payment Failed',
+              'email-payment-failed',
+              false
+            )
+            Application.find({
+              where: { firstName: customer.metadata.firstName },
+              sort: 'createdAt DESC',
+            })
+              .then(async (application) => {
+                await Application.updateOne({ id: application[0].id }).set({
+                  status: 'rejected',
+                })
+              })
+              .catch((err) => sails.log(err))
           })
-        } else {
-          // Handle case where no matching record is found
-        }
         break
       default:
         console.log(`Unhandled event type ${event.type}`)
